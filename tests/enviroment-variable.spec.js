b172de0ef61add4aec764e7d516ac5e823971571
zolimov <zolimov@immunetolerance.org>
2025-04-16 23:21:02 -0400

    updated env variables.

--- tests/enviroment-variable.spec.js
+++ tests/enviroment-variable.spec.js
@@ -1,16 +1,22 @@
 import { test } from '@playwright/test';
 
-test('@env-test Testing environment variable', async ({ page }) => {
-    
-    console.log(process.env.PRACTICE_USERNAME)
-    console.log(process.env.PRACTICE_PASSWORD)
-    
+test('@env-test Testing environment variables', async ({ page }) => {
+
+
+    console.log(`Username is: ${process.env.PRACTICE_USERNAME}`);
+    console.log(`Password is: ${process.env.PRACTICE_PASSWORD}`);
+
 });
+
 
 test('Bypass authentication by encoding the credentials base64 format', async ({ page }) => {
     
-    
-    
-    
-});
+    let encodedCredential = Buffer.from(`${process.env.PRACTICE_USERNAME}:${process.env.PRACTICE_PASSWORD}`).toString("base64");
 
+    await page.setExtraHTTPHeaders({'Authorization': `Basic ${encodedCredential}`});
+
+    page.goto("https://practice.cydeo.com/basic_auth");
+
+    await page.waitForTimeout(3000);
+
+});
